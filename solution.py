d50f16c17792cffaafbc568de870e5ac272efe93
Nadia Rafique <eesahayat14@gmail.com>
2026-02-14 16:06:05 +0500

    this is part 2

--- solution.py
+++ solution.py
@@ -16,5 +16,19 @@ def list_games(game_log):
             unique_games[name].append(game_name)
     print(unique_games)
     return unique_games
+games = list_games(game_log)
 
-list_games(game_log)
+def find_active_gamers(games_by_student, min_games):
+    #Return a list of students who have played at least min_games different games.
+    print(games_by_student)
+    active_gamers = []
+    for student,games in games_by_student.items():
+        print(student)
+        print(games)
+        if len(games) >= min_games:
+            active_gamers.append(student)
+    print(active_gamers)
+    return active_gamers
+
+            
+find_active_gamers(games,2)
