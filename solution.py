37048155432242e8bb55a8243998098cb9d3fa4b
Nadia Rafique <eesahayat14@gmail.com>
2026-02-14 16:39:11 +0500

    part 4

--- solution.py
+++ solution.py
@@ -16,13 +16,12 @@ def list_games(game_log):
             unique_games[name].append(game_name)
     print(unique_games)
     return unique_games
-games = list_games(game_log)
 
 def find_active_gamers(games_by_student, min_games):
     #Return a list of students who have played at least min_games different games.
     print(games_by_student)
     active_gamers = []
-    for student,games in games_by_student.items():
+    for student,games in games_by_student:
         print(student)
         print(games)
         if len(games) >= min_games:
@@ -31,4 +30,25 @@ def find_active_gamers(games_by_student, min_games):
     return active_gamers
 
             
-find_active_gamers(games,2)
+
+#Write a function:
+#Return a dictionary where:
+#	•	key → student name
+#	•	value → total minutes played
+def total_play_time(game_log):
+    minutes_played = 0 
+    playtime = {}
+    for logs in game_log:
+        current_name = logs["studentName"]
+        if  current_name not in playtime:
+            playtime[current_name] = 0
+        playtime[current_name] += logs["minutesPlayed"]
+    print(playtime)
+    return playtime
+
+
+def main():
+    #games = list_games(game_log)
+    #find_active_gamers(games,2)
+    total_play_time(game_log)
+main()
